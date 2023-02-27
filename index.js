b4779c5fc7ab14fecda0dc256f8f83a2e166116f
Tom <tpence06@hotmail.com>
2023-02-27 10:06:00 -0700

    pass

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+];
+function destructivelyAppendCat(Ralph) {
+    console.log(cats.push('Ralph'))
+};
+function destructivelyPrependCat(Bob) {
+    console.log(cats.unshift('Bob'))
+};
+function destructivelyRemoveLastCat() {
+    console.log(cats.pop())
+};
+function destructivelyRemoveFirstCat() {
+    console.log(cats.shift())
+};
+function appendCat(Broom) {
+    let newArray = [...cats]
+    newArray.push('Broom')
+    return newArray
+}
+function prependCat(Arnold) {
+    let newArr = [...cats]
+    newArr.unshift('Arnold')
+    return newArr
+}
+function removeLastCat(name) {
+    let newArray = [...cats];
+    newArray.pop();
+    return(newArray)
+};
+function removeFirstCat(name) {
+    let newArray = [...cats];
+    newArray.shift();
+    return(newArray)
+};
